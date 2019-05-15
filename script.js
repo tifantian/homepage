c238d7814f3dee4d7467a9532feed0d6ef7a4755
Kirill <burik0304@icloud.com>
2019-05-16 01:21:36 +0300

    Improve script js with feature light

--- script.js
+++ script.js
@@ -1,33 +1,78 @@
-// new file
-var scrollHeight = Math.max(
+// javaScript code for extra-feature
+let scrollHeight = Math.max(
 	document.body.scrollHeight, document.documentElement.scrollHeight,
 	document.body.offsetHeight, document.documentElement.offsetHeight,
 	document.body.clientHeight, document.documentElement.clientHeight
 );
-// var torch = document.getElementById('moon');
-// torch.style.height = scrollHeight + 'px';
-// document.body.addEventListener('mousemove', function(e) {
-// 	var top = e.pageY;
-// 	var left = e.pageX;
-	
-// 	torch.style.clip = "rect("+(top-100)+'px,'+left+'px,'+top+'px,'+(left-100)+"px)";
-// });
 
-// window.onscroll = function() {
-// 	var scrolled = window.pageYOffset || document.documentElement.scrollTop;  
-// }
+const cach = {
+    pageX:0,
+    pageY:0,
+    yPosition:0,
+    offset:0
+}
 
+
+let extra = document.getElementsByClassName('extra-feature')[0];
+let position = extra.getBoundingClientRect();
+console.log(position)
+let element = document.getElementsByClassName('child-spotlight')[0];
+let posTop = position.top + 50,
+    posLeft = position.left + 14;
+element.style.cssText = `width: ${position.width}px; height: ${position.height}px; position: absolute; top: ${posTop}px; left: ${posLeft}px`;
+
+
+let isFirst = true;
+let speed = 20;
 const spotlight = document.querySelector(".spotlight");
 spotlight.style.height = scrollHeight + 'px';
-    let spotlightSize = "transparent 80px, rgba(0, 0, 0, 0.95) 110px)";
+let spotlightSize = "transparent 0px, rgba(0, 0, 0, 1) 0px)";
 
-    window.addEventListener("mousemove", e => {
-      requestAnimationFrame(() => updateSpotlight(e));
-    });
+window.addEventListener("mousemove", e => {
+    requestAnimationFrame(() => updateSpotlight(e));
+});
 
-    function updateSpotlight(e) {
-      const xPos = e.pageX / window.innerWidth * 100;
-	  const yPos = e.pageY / scrollHeight * 100;
-	  console.log(xPos, yPos);
-      spotlight.style.backgroundImage = `radial-gradient(circle at ${xPos}% ${yPos}%, ${spotlightSize}`;
+window.addEventListener("scroll", ()=>{
+    cach.pageY = cach.yPosition + window.pageYOffset;
+    cach.offset =  window.pageYOffset;
+    requestAnimationFrame(() => updateSpotlight(cach,"scroll"));
+})
+
+function updateSpotlight(e,event) {
+    const xPos = e.pageX / window.innerWidth * 100;
+    const yPos = e.pageY  / scrollHeight  * 100 ;
+    if(event!="scroll"){
+    cach.pageX = e.pageX;
+    cach.pageY = e.pageY;
+    cach.yPosition = e.pageY - cach.offset;
     }
+    spotlight.style.backgroundImage = `radial-gradient(circle at ${xPos}% ${yPos}%, ${spotlightSize}`;
+}
+element.addEventListener('click', function() {
+    sunflower.style.animationName = 'wind-power';
+    spotlight.style.animationName = 'wind-power-racing';
+    setTimeout(function() {
+        spotlight.style.display = 'none';   
+        document.body.style.cursor = '';
+        childSpotLight.style.display = 'none';
+        spotlightSize = "transparent 0px, rgba(0, 0, 0, 1) 0px)";
+    }, 1980);
+});
+
+childSpotLight = document.getElementsByClassName('child-spotlight')[0];
+function extraFeature() {
+    spotlight.style.display = 'block';
+    spotlight.style.animationName = 'wind-power-racing-on';
+    document.body.style.cursor = 'none';
+    setTimeout(function() {
+        spotlightSize = "transparent 80px, rgba(0, 0, 0, 1) 110px)";
+        sunflower.style.animationName = 'none';
+        childSpotLight.style.display = 'block';
+    }, 2000);
+}
+
+
+
+
+
+
